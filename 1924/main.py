0df0c289c69973ff8736f33c1062e0872627ccd1
yunhosong <songyh1997@naver.com>
2020-10-24 10:16:42 +0900

    백준 처음 등록

---
+++ 1924/main.py
@@ -0,0 +1,20 @@
+import sys
+
+#f = sys.stdin
+week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', "SAT"]
+
+f = open('data.txt', 'r')
+M, D = map(int, f.readline().split())
+day = D
+
+for m in range(1,13):
+    if m==M:
+        break
+    if m in [1, 3, 5, 7, 8, 10, 12]:
+        day += 31
+    elif m in [4, 6, 9, 11]:
+        day += 30
+    else:
+        day += 28
+
+print (week[day%7])
