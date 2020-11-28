c95f209399811d4befb0da44055af5608c9635a4
yunhosong <songyh1997@naver.com>
2020-11-28 14:49:32 +0900

    20201128 디버깅 완료 및 프로그램 완성

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -6,17 +6,16 @@
 # 길이가 정해진 리스트를 선언하고 그 리스트 전체를 0으로 초기화 하는 방법을 몰라 헤맸습니다.
 # 리스트 만들기 참고 https://jobc.tistory.com/m/141, 리스트 값 바꾸기 참고 https://aenam00.tistory.com/51
 # 4주차  20201120 코딩 마무리 및 디버깅
+# 5주차 디버깅 완료 및 프로그램 완성
 
 
 import sys
-#f = sys.stdin
-#f = open('data.txt', 'r')
 
 I = 1
-W, Out = 0, 0
-Table = [0 for i in range(101)]
+W, Out = 0, 0 # W = 원하는 자리, Out = 거절당한 손님
+Table = [0 for i in range(101)] # Table = PC방 자리
 
-N = int(input("손님 수를 입력해주세요(1~100): "))
+N = int(input("손님 수를 입력해주세요(1~100): ")) # N = 손님 수
 
 while N > 100:
     N = int(input("1~100 사이를 입력해주세요: "))
